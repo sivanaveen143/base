f5445bf6a9cb16d0de049eabf884b641cf84f98d
sivanaveen143 <sivanaveenyadav777@gmail.com>
2022-09-08 23:56:57 +0530

    commit

--- Base_Project/Base_Project/settings.py
+++ Base_Project/Base_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-os$+#-$9d5a22dfrudt7vr&v!u6n+#3@pj-4_i4uvsige@@^k%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['test.com','192.168.1.5']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
