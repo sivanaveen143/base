5a9e8779eae8f364ef1bef97526bc4edce0c6d52
sivanaveen143 <sivanaveenyadav777@gmail.com>
2022-09-09 11:27:08 +0530

    Commit

--- Base_Project/Base_Project/wsgi.py
+++ Base_Project/Base_Project/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import os 
+import sys 
+
+DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+sys.path.append(DJANGO_PATH)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Base_Project.settings')
 
