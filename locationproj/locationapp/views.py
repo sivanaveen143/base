1cb93b67caa316ca57568c0184c59b23454112b7
sivanaveen143 <sivanaveenyadav777@gmail.com>
2022-10-05 00:14:38 +0530

    first

--- locationproj/locationapp/views.py
+++ locationproj/locationapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.gis.geoip2 import GeoIP2
 # Create your views here.
 def index(request):
-    
+    x_forwarded = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded:
+        ip = x_forwarded.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    g = GeoIP2()
+    lat, lon = g.lat_lon(ip)
+    print(lat, lon)
+    print(ip)
     return render(request, "index.html")
--- locationproj/locationproj/settings.py
+++ locationproj/locationproj/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import whitenoise
+import whitenoise,os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+GEOIP_PATH = os.path.join('geoip')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
