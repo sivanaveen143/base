1b0bcf16606ce500ad3fa9b488777aa15f6f5db2
sivanaveen143 <sivanaveenyadav777@gmail.com>
2022-10-05 00:56:03 +0530

    ..

--- locationproj/locationapp/views.py
+++ locationproj/locationapp/views.py
@@ -11,4 +11,4 @@ def index(request):
     lat, lon = g.lat_lon(ip)
     print(lat, lon)
     print(ip)
-    return render(request, "index.html")
+    return render(request, "index.html",{"lon" : lon,"lat":lat})
