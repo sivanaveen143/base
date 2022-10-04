d3d6794ed8eb935c70b400bd7ac99a8fcfc87a95
sivanaveen143 <sivanaveenyadav777@gmail.com>
2022-10-04 11:22:21 +0530

    ...

--- Base_Project/chat/models.py
+++
@@ -1,7 +0,0 @@
-from email import message
-from django.db import models
-
-# Create your models here.
-class chat(models.Model):
-    username = models.CharField(max_length=20)
-    message = models.CharField(max_length=100)
