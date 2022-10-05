5bef5d2a3a2e6b67f770199736b52a0d1aaf53ed
sivanaveen143 <sivanaveenyadav777@gmail.com>
2022-10-05 12:34:43 +0530

    ...

--- locationproj/static/javascript/location.js
+++ locationproj/static/javascript/location.js
@@ -6,9 +6,10 @@ if (window.navigator) {
 const successcallback = (position) => {
     console.log(position);
     const { latitude, longitude } = position.coords;
+    
+}
     document.getElementById("coor1").innerHTML = longitude;
     document.getElementById("coor2").innerHTML = latitude;
     document.getElementById("pos").innerHTML = position;
     document.getElementById("update").innerHTML = "1";
-}
 }
