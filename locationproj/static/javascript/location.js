685073b37cc85a30350f869a54fa00b81827b6e6
sivanaveen143 <sivanaveenyadav777@gmail.com>
2022-10-05 12:45:53 +0530

    ..

--- locationproj/static/javascript/location.js
+++ locationproj/static/javascript/location.js
@@ -1,13 +1,17 @@
-function clicked(){
-    alert("running....");
+
 if (window.navigator) {
     window.navigator.geolocation.getCurrentPosition(successcallback, failurecallback);
 }
 const successcallback = (position) => {
     console.log(position);
     const { latitude, longitude } = position.coords;
+    console.log("longitude");
+    console.log(longitude);
     
 }
+
+function clicked(){
+    alert("running....");
     document.getElementById("coor1").innerHTML = longitude;
     document.getElementById("coor2").innerHTML = latitude;
     document.getElementById("pos").innerHTML = position;
